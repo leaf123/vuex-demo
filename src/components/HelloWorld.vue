b7e883aa07be1bbed356ef182035cfc2779e5a36
leaf <1264456658@qq.com>
2019-03-11 14:41:42 +0800

    第二次提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+   <!-- <product-list-one v-bind:products="products"></product-list-one>
+   <product-list-two v-bind:products="products"></product-list-two> -->
+   <product-list-one></product-list-one>
+   <product-list-two></product-list-two>
+  </div>
+</template>
+<script>
+import ProductListOne from './ProductListOne.vue'
+import ProductListTwo from './ProductListTwo.vue'
+export default {
+  name: 'HelloWorld',
+  components:{
+    'product-list-one':ProductListOne,
+    'product-list-two':ProductListTwo
+  },
+  data () {
+    return {
+     
+    }
+  },
+  //  computed: {
+  //   count () {
+  //     return this.$store.state.count
+  //   }
+  // },
+  // computed: mapState({
+  //   // 箭头函数可使代码更简练
+  //   count: state => state.count,
+
+  //   // 传字符串参数 'count' 等同于 `state => state.count`
+  //   countAlias: 'count',
+
+  //   // 为了能够使用 `this` 获取局部状态，必须使用常规函数
+  //   countPlusLocalState (state) {
+  //     return state.count + this.localCount
+  //   }
+  // })
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/ProductListTwo.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="ProductListTwo">
+      <h2>Product List Two</h2>
+      <ul>
+        <li v-for="product in saleProducts">
+            <span class="name">{{product.name}}</span>
+            <span class="price">${{product.price}}</span>
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+// import { mapState } from 'vuex'
+import {mapGetters} from 'vuex'
+import {mapActions} from 'vuex'
+export default {
+    computed:{
+        // products(){
+        //     return this.$store.state.products;
+        // },
+        // saleProducts(){
+        //     return this.$store.getters.saleProducts;
+        // }
+         ...mapGetters([
+            'products',
+            'saleProducts'
+        ])
+    },
+    methods:{
+       /* reducePrice:function(amount){
+            // this.$store.state.products.forEach(product =>{
+            //     product.price -= 1;
+            // })
+           // this.$store.commit('reducePrice')
+            this.$store.dispatch('reducePrice',amount);
+        }*/
+        // ...mapActions([
+        //     'reducePrice'
+        // ])
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .ProductListTwo{
+    background: #D1E4FF;
+    box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
+    margin-bottom: 30px;
+    padding: 10px 20px;
+    }
+    .ProductListTwo ul{
+        padding: 0;
+        list-style-type: none;
+    }
+    .ProductListTwo li{
+        margin-right: 10px;
+        margin-top: 10px;
+        padding: 20px;
+        background: rgba(255,255,255,0.7);
+    }
+    .price{
+        font-weight: bold;
+        color: #860CE8;
+        display: block;
+    }
+</style>
